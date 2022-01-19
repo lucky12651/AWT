ad5cd0e2a122a82c6e164f59557225ea2cc7930e
Vaibhav Srivastava <vkluckymgs@gmail.com>
2022-01-19 23:05:38 +0530

    new

---
+++ login.js
@@ -0,0 +1,11 @@
+var name ="vaibhav";
+var namet="b";
+var nameb="h";
+var response;
+
+
+while(response != name && response !=namet && response !=nameb){
+          response = window.prompt("Enter Name");
+}
+          
+     alert("Sucess");
